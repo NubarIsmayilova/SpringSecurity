52366afcbc43dd9af4c64444c81b368b340d5092
Nubar Ismayilova <nubar.ismayilova@yahoo.com>
2024-04-02 01:24:26 +0300

    Initial commit

---
+++ src/main/java/com/company/springsecurity/controller/SecurityController.java
@@ -0,0 +1,15 @@
+package com.company.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/security")
+public class SecurityController {
+
+    @GetMapping("/hello")
+    public String sayHello () {
+        return "Hello Security";
+    }
+}
---
+++ src/main/java/com/company/springsecurity/service/SecurityService.java
@@ -0,0 +1,4 @@
+package com.company.springsecurity.service;
+
+public class SecurityService {
+}
